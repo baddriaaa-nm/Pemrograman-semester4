67c346d766042f839095cc5d3d8a1bafc6f434ae
baddriaaa-nm <baddriaaa@gmail.com>
2026-04-30 16:08:46 +0700

    Delete P1_BADRIAH directory

--- P1_BADRIAH/Myself.java
+++
@@ -1,7 +0,0 @@
-public class Myself {
-    public static void main(String[] args) {
-        System.out.println("Nama    : Badriah");
-        System.out.println("NIM     : 241011400384");
-        System.out.println("Jurusan : Teknik Informatika");
-    }
-}
